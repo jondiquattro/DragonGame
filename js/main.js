af68629bf12ccdd01dde2c932af1ebdaf368db91
Jonathan DiQuattro <jondiquattro@hotmail.com>
2018-10-03 11:21:33 -0700

    Added css sheet

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+
+//the title of this file is main.js but that is a poor name because it is 
+//in rfrence to this being the main js file that glues everything together
+//not to be confused with stateMain.js which is the main gaming screen
+
+
 var game;
 var score;
 var soundOn = true;
@@ -9,10 +15,14 @@ window.onload = function () {
 
     } else {
         //mobile device
-        game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, "ph_game");
+        game = new 
+        Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, "ph_game");
     }
     //add a state or screen to the game
     game.state.add("StateMain", StateMain);
-    game.state.start("StateMain");
+    game.state.add("StateTitle", StateTitle);
+
+    //chooses which screen to start with
+    game.state.start("StateTitle");
 
 }
